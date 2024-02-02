3ba67bd71b31800444c38d557b3398a92e3d17f2
sagnik6969 <72630181+sagnik6969@users.noreply.github.com>
2024-02-02 10:32:02 +0530

    90 delete user

--- src/views/user/User.spec.js
+++ src/views/user/User.spec.js
@@ -1,13 +1,17 @@
 import { screen, waitFor } from '@testing-library/vue'
-import { HttpResponse, http } from 'msw'
+import { HttpResponse, delay, http } from 'msw'
 import { setupServer } from 'msw/node'
-import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it, vi } from 'vitest'
 import User from './User.vue'
 import { router, render } from '../../../test/helper'
 import { i18n } from '@/locales'
+import userEvent from '@testing-library/user-event'
 
 let counter = 0
 let id
+
+let userDeleteId
+let userDeleteCounter = 0
 const server = setupServer(
   http.get('/api/v1/users/:id', ({ params }) => {
     id = params.id
@@ -19,13 +23,25 @@ const server = setupServer(
       email: 'user1@mail.com',
       image: null
     })
+  }),
+  http.delete('/api/v1/users/:id', ({ params }) => {
+    userDeleteCounter++
+    userDeleteId = params.id
+
+    return HttpResponse.json({})
   })
 )
 
 beforeAll(() => server.listen())
 afterAll(() => server.close())
 
-beforeEach(() => server.resetHandlers())
+beforeEach(() => {
+  server.resetHandlers()
+  id = undefined
+  counter = 0
+  userDeleteId = undefined
+  userDeleteCounter = 0
+})
 
 describe('user page', () => {
   it('sends request to the server', async () => {
@@ -180,4 +196,341 @@ describe('user page', () => {
       })
     }
   )
+
+  describe('when user is logged in', () => {
+    it('does not display spinner', async () => {
+      localStorage.setItem('auth', JSON.stringify({ id: 1, username: 'user3' }))
+
+      router.push(`/user/1`)
+      await router.isReady()
+      render(User)
+
+      await waitFor(() => {
+        expect(screen.queryByRole('status')).not.toBeInTheDocument()
+      })
+    })
+
+    describe('when profile card is of logged in user', () => {
+      it('displays delete button', async () => {
+        localStorage.setItem('auth', JSON.stringify({ id: 1, username: 'user3' }))
+
+        router.push(`/user/1`)
+        await router.isReady()
+        render(User)
+        await waitFor(() => {
+          expect(screen.getByRole('button', { name: 'Delete' })).toBeInTheDocument()
+        })
+      })
+    })
+
+    describe('when profile card is not of logged in user', () => {
+      it('does not display delete button', async () => {
+        localStorage.setItem('auth', JSON.stringify({ id: 2, username: 'user3' }))
+
+        router.push(`/user/1`)
+        await router.isReady()
+        render(User)
+        await waitFor(() => {
+          expect(screen.queryByRole('button', { name: 'Delete' })).not.toBeInTheDocument()
+        })
+      })
+    })
+  })
+
+  describe('when user is not logged in', () => {
+    it('does not show delete button', async () => {
+      router.push(`/user/1`)
+      await router.isReady()
+      render(User)
+      await waitFor(() => {
+        expect(screen.queryByRole('button', { name: 'Delete' })).not.toBeInTheDocument()
+      })
+    })
+  })
+
+  describe('when user clicks delete button', () => {
+    it('displays confirmation dialog', async () => {
+      vi.spyOn(window, 'confirm').mockReturnValue(0)
+
+      localStorage.setItem('auth', JSON.stringify({ id: 1, username: 'user3' }))
+
+      const user = userEvent.setup()
+      router.push(`/user/1`)
+      await router.isReady()
+      render(User)
+
+      const deleteButton = await screen.findByRole('button', { name: 'Delete' })
+      await user.click(deleteButton)
+
+      await waitFor(() => {
+        expect(window.confirm).toHaveBeenCalledTimes(1)
+        // we can call the above function only when window.confirm is bound by vi.spyOn()
+      })
+    })
+
+    // describe('when user cancels do nothing', () => {})
+
+    describe('when user clicks ok', () => {
+      it('sends delete request to server', async () => {
+        vi.spyOn(window, 'confirm').mockReturnValue(1)
+
+        localStorage.setItem('auth', JSON.stringify({ id: 1, username: 'user3' }))
+
+        const user = userEvent.setup()
+        router.push(`/user/1`)
+        await router.isReady()
+        render(User)
+
+        const deleteButton = await screen.findByRole('button', { name: 'Delete' })
+        await user.click(deleteButton)
+
+        await waitFor(() => {
+          expect(userDeleteCounter).toBe(1)
+        })
+      })
+
+      it('sends request to server with appropriate user id', async () => {
+        server.use(
+          http.get('/api/v1/users/:id', ({ params }) => {
+            id = params.id
+            counter++
+
+            return HttpResponse.json({
+              id: 3,
+              username: 'user1',
+              email: 'user1@mail.com',
+              image: null
+            })
+          })
+        )
+
+        vi.spyOn(window, 'confirm').mockReturnValue(1)
+
+        localStorage.setItem('auth', JSON.stringify({ id: 3, username: 'user3' }))
+
+        const user = userEvent.setup()
+        router.push(`/user/3`)
+        await router.isReady()
+        render(User)
+
+        const deleteButton = await screen.findByRole('button', { name: 'Delete' })
+        await user.click(deleteButton)
+
+        await waitFor(() => {
+          expect(userDeleteCounter).toBe(1)
+          expect(userDeleteId).toBe('3')
+        })
+      })
+
+      describe('when there is an ongoing api call', () => {
+        it('displays spinner', async () => {
+          server.use(
+            http.delete('/api/v1/users/:id', async ({ params }) => {
+              await delay('infinite')
+            })
+          )
+
+          vi.spyOn(window, 'confirm').mockReturnValue(1)
+
+          localStorage.setItem('auth', JSON.stringify({ id: 1, username: 'user1' }))
+
+          const user = userEvent.setup()
+          router.push(`/user/1`)
+          await router.isReady()
+          render(User)
+
+          const deleteButton = await screen.findByRole('button', { name: 'Delete' })
+          await user.click(deleteButton)
+
+          expect(screen.getByRole('status')).toBeInTheDocument()
+        })
+
+        it('disables the delete button', async () => {
+          server.use(
+            http.delete('/api/v1/users/:id', async ({ params }) => {
+              await delay('infinite')
+            })
+          )
+
+          vi.spyOn(window, 'confirm').mockReturnValue(1)
+
+          localStorage.setItem('auth', JSON.stringify({ id: 1, username: 'user1' }))
+
+          const user = userEvent.setup()
+          router.push(`/user/1`)
+          await router.isReady()
+          render(User)
+
+          const deleteButton = await screen.findByRole('button', { name: 'Delete' })
+          await user.click(deleteButton)
+
+          expect(deleteButton).toBeDisabled()
+        })
+      })
+
+      describe('when request fails with network error', () => {
+        it('displays generic error', async () => {
+          server.use(
+            http.delete('/api/v1/users/:id', async ({ params }) => {
+              return HttpResponse.error()
+            })
+          )
+
+          vi.spyOn(window, 'confirm').mockReturnValue(1)
+
+          localStorage.setItem('auth', JSON.stringify({ id: 1, username: 'user1' }))
+
+          const user = userEvent.setup()
+          router.push(`/user/1`)
+          await router.isReady()
+          render(User)
+
+          const deleteButton = await screen.findByRole('button', { name: 'Delete' })
+          await user.click(deleteButton)
+
+          await waitFor(() => {
+            expect(
+              screen.getByText('Unexpected error occurred, please try again')
+            ).toBeInTheDocument()
+          })
+        })
+
+        it('enables button', async () => {
+          server.use(
+            http.delete('/api/v1/users/:id', async ({ params }) => {
+              return HttpResponse.error()
+            })
+          )
+
+          vi.spyOn(window, 'confirm').mockReturnValue(1)
+
+          localStorage.setItem('auth', JSON.stringify({ id: 1, username: 'user1' }))
+
+          const user = userEvent.setup()
+          router.push(`/user/1`)
+          await router.isReady()
+          render(User)
+
+          const deleteButton = await screen.findByRole('button', { name: 'Delete' })
+          await user.click(deleteButton)
+
+          await waitFor(() => {
+            expect(deleteButton).toBeEnabled()
+          })
+        })
+
+        it('hides the spinner', async () => {
+          server.use(
+            http.delete('/api/v1/users/:id', async ({ params }) => {
+              return HttpResponse.error()
+            })
+          )
+
+          vi.spyOn(window, 'confirm').mockReturnValue(1)
+
+          localStorage.setItem('auth', JSON.stringify({ id: 1, username: 'user1' }))
+
+          const user = userEvent.setup()
+          router.push(`/user/1`)
+          await router.isReady()
+          render(User)
+
+          const deleteButton = await screen.findByRole('button', { name: 'Delete' })
+          await user.click(deleteButton)
+          // expect(screen.queryByRole('status')).toBeInTheDocument()
+
+          await waitFor(() => {
+            expect(screen.queryByRole('status')).not.toBeInTheDocument()
+          })
+        })
+
+        describe('when user submits again', () => {
+          it('hides the error', async () => {
+            let firstRequestDone = false
+
+            server.use(
+              http.delete('/api/v1/users/:id', async ({ params }) => {
+                if (firstRequestDone == false) {
+                  firstRequestDone = true
+                  return HttpResponse.error()
+                } else {
+                  await delay('infinite')
+                }
+              })
+            )
+
+            vi.spyOn(window, 'confirm').mockReturnValue(1)
+
+            localStorage.setItem('auth', JSON.stringify({ id: 1, username: 'user1' }))
+
+            const user = userEvent.setup()
+            router.push(`/user/1`)
+            await router.isReady()
+            render(User)
+
+            const deleteButton = await screen.findByRole('button', { name: 'Delete' })
+            await user.click(deleteButton)
+
+            const error = await screen.findByText('Unexpected error occurred, please try again')
+
+            await user.click(deleteButton)
+
+            expect(error).not.toBeInTheDocument()
+
+            // expect(screen.queryByRole('status')).toBeInTheDocument()
+          })
+        })
+      })
+
+      describe('when backend returns success', () => {
+        it('navigates to home', async () => {
+          server.use(
+            http.delete('/api/v1/users/:id', async ({ params }) => {
+              return HttpResponse.json({ message: 'success' })
+            })
+          )
+
+          vi.spyOn(window, 'confirm').mockReturnValue(1)
+
+          localStorage.setItem('auth', JSON.stringify({ id: 1, username: 'user1' }))
+
+          const user = userEvent.setup()
+          router.push(`/user/1`)
+          await router.isReady()
+          render(User)
+
+          const deleteButton = await screen.findByRole('button', { name: 'Delete' })
+          await user.click(deleteButton)
+
+          await waitFor(() => {
+            expect(router.currentRoute.value.fullPath).toBe('/')
+          })
+        })
+
+        it('removes user from local storage', async () => {
+          server.use(
+            http.delete('/api/v1/users/:id', async ({ params }) => {
+              return HttpResponse.json({ message: 'success' })
+            })
+          )
+
+          vi.spyOn(window, 'confirm').mockReturnValue(1)
+
+          localStorage.setItem('auth', JSON.stringify({ id: 1, username: 'user1' }))
+
+          const user = userEvent.setup()
+          router.push(`/user/1`)
+          await router.isReady()
+          render(User)
+
+          const deleteButton = await screen.findByRole('button', { name: 'Delete' })
+          await user.click(deleteButton)
+
+          await waitFor(() => {
+            expect(JSON.parse(localStorage.getItem('auth')).id).toBe(0)
+          })
+        })
+      })
+    })
+  })
 })
--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -3,7 +3,13 @@
     <Card>
       <template v-slot:header> User Page </template>
       <template v-slot:default> <img src="@/assets/profile.png" alt="" /> </template>
-      <template v-if="status === 'success'" v-slot:footer> {{ data.username }}</template>
+      <template v-if="status === 'success'" v-slot:footer>
+        {{ data.username }}
+        <button v-if="auth.id === data.id" @click="deleteUser" :disabled="apiProcessing">
+          Delete
+        </button>
+        <span v-if="apiProcessing" role="status" class="spinner-border spinner-border-sm"></span>
+      </template>
     </Card>
     <div v-if="status === 'loading'">
       <span role="status" class="spinner-border spinner-border-sm"></span>
@@ -11,17 +17,45 @@
     <div v-if="status === 'fail'" class="alert alert-success">
       {{ error }}
     </div>
+    <div v-if="deleteUserError" class="alert alert-success">
+      {{ deleteUserError }}
+    </div>
   </div>
 </template>
 
 <script setup>
 import Card from '@/components/Card.vue'
 import http from '@/lib/http'
-
+const { auth, logout } = useAuthStore()
 const getUserById = async (id) => {
   return await http.get(`/api/v1/users/${id}`)
 }
 
 import useRouteParamApiRequest from '@/shared/useRouteParamApiRequest.js'
+import { useAuthStore } from '@/stores/auth'
+import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useRouter } from 'vue-router'
+// import { router } from '../../../test/helper'
 const { status, data, error } = useRouteParamApiRequest(getUserById, 'id')
+const { t } = useI18n()
+const router = useRouter()
+const apiProcessing = ref(false)
+const deleteUserError = ref('')
+const deleteUser = async () => {
+  if (confirm('are you sure')) {
+    apiProcessing.value = true
+    deleteUserError.value = ''
+
+    try {
+      await http.delete(`/api/v1/users/${data.value.id}`)
+      logout()
+      router.push('/')
+    } catch (error) {
+      deleteUserError.value = t('genericError')
+    }
+
+    apiProcessing.value = false
+  }
+}
 </script>
